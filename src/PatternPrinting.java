3e2c61ce2a487d006484c1c70d318bc9dd0d2ee4
Adithyan <adithyanmv2006@gmail.com>
2026-01-15 18:13:40 +0530

    added PatternPrinting code

--- src/PatternPrinting.java
+++ src/PatternPrinting.java
@@ -1,5 +1,17 @@
 import java.util.Scanner;
 
 public class PatternPrinting {
-    
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		int n;
+		n=sc.nextInt();
+		for(int i=0;i<n;i++){
+			System.out.print("* ");
+		}	
+		System.out.println(" ");
+		for(int i=0;i<n;i++){
+			System.out.println("*");
+		}
+
+	} 
 }
