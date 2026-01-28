1158dd07b98cadda887bc4cc83e9b160fd93846d
Adityan M V <adithyanmv2006@gmail.com>
2026-01-28 14:52:59 +0530

    error updated

--- src/PatternPrinting.java
+++ src/PatternPrinting.java
@@ -1,22 +1,29 @@
 import java.util.Scanner;
 
 public class PatternPrinting {
-	public static void main(String[] args){
-		Scanner sc=new Scanner(System.in);
+ public static void main(String args[]){
+	
+		Scanner sc = new Scanner(System.in);
+
 		int n;
-		n=sc.nextInt();
-		for(int i=0;i<n;i++){	
-			System.out.print("* ");
-		}	
-		System.out.println(" ");
-		for(int i=0;i<n;i++){
-			if(i==n-1){
-				System.out.print("*");
-			}
-			else{	
-				System.out.println("* ");
-			}
+	
+		n = sc.nextInt();
+
+		for(int i=0;i<n;i++)
+		{
+            if(i==n-1)
+                System.out.println("*");
+            else
+			    System.out.print("* ");
 		}
 
-	} 
+		for(int i=0;i<n;i++)
+		{
+			System.out.println("*");
+		}
+
+        sc.close();
+
+	}
+    
 }
